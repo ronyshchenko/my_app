47337e77c0dedb0069ba6938eda9314ca1ddc7e5
ronyshchenko <ronyshchenko@gmail.com>
2019-07-29 14:45:53 +0300

    Updates

---
+++ precache-manifest.b98b3e78a12e5cebf906280530138bcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b877b7c8f201ccc2684d5d25b4057ef",
+    "url": "/my_app/index.html"
+  },
+  {
+    "revision": "4eeee79150c6c97471d1",
+    "url": "/my_app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/my_app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4eeee79150c6c97471d1",
+    "url": "/my_app/static/js/main.08db46e4.chunk.js"
+  },
+  {
+    "revision": "25c9988c66468b3bdff7",
+    "url": "/my_app/static/js/runtime~main.06f84023.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
